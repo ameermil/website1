857a51014ae84dacd4b27fccf3e7ade6db2de379
ameermil <117845667+ameermil@users.noreply.github.com>
2023-05-20 14:23:23 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,36 @@
+export let plansList = [
+	{
+		//####################################### start updating as needed #######################################
+		name: "normal",
+		monthlySubPrice: 0,
+		yearlySubPrice: 0,
+		numericFeature1: 0,
+		numericFeature2: 0,
+		binaryFeature1: false,
+		binaryFeature2: false,
+		customerCare: false
+	},
+	{
+		name: "pro",
+		monthlySubPrice: 0,
+		yearlySubPrice: 0,
+		numericFeature1: 0,
+		numericFeature2: 0,
+		binaryFeature1: true,
+		binaryFeature2: false,
+		customerCare: "By Email"
+	},
+	{
+		name: "premium",
+		monthlySubPrice: 0,
+		yearlySubPrice: 0,
+		numericFeature1: 0,
+		numericFeature2: 0,
+		binaryFeature1: true,
+		binaryFeature2: true,
+		customerCare: "24/7 Line"
+		//#######################################      stop  updating      #######################################
+		                                        
+	}
+]
+
